2cc9632f9a38c360aed423d533e127152f16907c
SuzanneVZ <vanzuijlen.suzanne@gmail.com>
2021-07-26 15:56:08 +0200

    Ajout d'un client

--- src/main/java/metier/Adresse.java
+++ src/main/java/metier/Adresse.java
@@ -10,7 +10,6 @@ public class Adresse {
 		this.rue = rue;
 		this.ville = ville;
 		
-
 	}
 
 	@Override
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import metier.Adresse;
 import metier.Boutique;
+import metier.Client;
 import metier.Console;
 import metier.Jeu;
 
@@ -27,6 +28,11 @@ public class Test {
 		catalogue.add(jeu1);
 		catalogue.add(jeu2);
 		catalogue.add(jeu3);
+		List<Jeu> MesJeuxC1 = new ArrayList<Jeu>();
+		MesJeuxC1.add(jeu1);
+		MesJeuxC1.add(jeu4);
+		MesJeuxC1.add(jeu2);
+		Client c1 = new Client("Mario","TheWinner",MesJeuxC1);
 
 		Boutique micromania = new Boutique("Micromania",adresse,catalogue) ;
 	}
